288d943b18a8777f713c6beeb7af7fe6f407c14f
kahurangi <kahurangi@axos.systems>
2025-03-30 19:38:26 +1300

    completion

--- ws.rb
+++ ws.rb
@@ -10,12 +10,11 @@ if __FILE__ == $0
 
   # Extract input file name and create output file name
   digits = input_file.match(/input-(\d{3})\.txt/)[1]
-  #TODO - Change the output file path to be in the correct dir
   output_file = File.join('output', "output-#{digits}.txt") # Write to output/ directory
 
   # Initialize stack
   stack = []
-  #test
+
   begin
     # Read input file
     File.open(input_file, 'r') do |file|
@@ -23,10 +22,9 @@ if __FILE__ == $0
         elements = line.split
         elements.each do |element|
           case element
-          when /\d+/ # Integer operand
+          when /\d+/ # Integer
             stack.push(element.to_i)
-          when '+', '-', '*', '/', '**', '%' # Operators
-            # Pop top two elements, perform operation, push result
+          when '+', '-', '*', '/', '**', '%'
             b = stack.pop
             a = stack.pop
             stack.push(a.send(element, b))
@@ -37,6 +35,47 @@ if __FILE__ == $0
           when 'SWAP'
             a, b = stack.pop(2)
             stack.push(b, a)
+          when 'ROT'
+            a, b, c = stack.pop(3)
+            stack.push(b, c, a)
+          when 'ROLL'
+            n = stack.pop
+            elements_to_rotate = stack.pop(n)
+            stack.push(*elements_to_rotate.rotate)
+          when 'ROLLD'
+            n = stack.pop
+            if n > 0 && n <= stack.length
+              elements_to_rotate = stack.pop(n)
+              stack.push(elements_to_rotate[-1], *elements_to_rotate[0..-2])
+            end
+          when 'IFELSE'
+            condition = stack.pop
+            false_case = stack.pop
+            true_case = stack.pop
+            if condition != 0 && condition != false
+              stack.push(true_case)
+            else
+              stack.push(false_case)
+            end
+          when '==', '!=', '>', '<', '>=', '<=', '<=>'
+            b = stack.pop
+            a = stack.pop
+            stack.push(a.send(element, b))
+          when '&', '|', '^'  # Logical operators (and, or, xor)
+            b = stack.pop
+            a = stack.pop
+            stack.push(a.send(element, b))
+          when 'true'
+            stack.push(true)
+          when 'false'
+            stack.push(false)
+          when '"', '"'  # String operands
+            string = stack.pop
+            stack.push(string)
+          when '<<', '>>'  # Bitshift operators
+            b = stack.pop
+            a = stack.pop
+            stack.push(a.send(element, b))
           else
             # Handle unknown elements (optional)
           end
@@ -50,6 +89,6 @@ if __FILE__ == $0
     end
   rescue => e
     # Handle errors gracefully
-    puts "Error: #{e.message}" # Remove this line in final submission
+    puts "Error: #{e.message}"
   end
 end
