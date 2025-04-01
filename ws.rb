95bc6a5de82fb797e4d8740eb5af1b125c8397b1
kahurangi <kahurangi@axos.systems>
2025-04-01 21:40:53 +1300

    migrate everything to a class and module

--- ws.rb
+++ ws.rb
@@ -1,10 +1,68 @@
+require 'matrix'
+
+class Calculator
+  attr_accessor :stack, :tokens
+  def initialize(tokens)
+    @tokens = tokens
+    @stack = []
+  end
+
+  def evaluate
+    while (element = @tokens.shift)
+      case element
+      when Integer
+        @stack.push(element)
+      when 'true'
+        @stack.push(true)
+      when 'false'
+        @stack.push(false)
+      when /\A".*"\z/
+        @stack.push(element[1..-2])
+      when '+', '-', '*', '/', '**', '%', '&', '|', '^', '<<', '>>', '!', '~'
+        Arithmetic.handle_operation(@stack, element)
+      when 'DROP'
+        StackManipulation.drop(@stack)
+      when 'DUP'
+        StackManipulation.dup(@stack)
+      when 'SWAP'
+        StackManipulation.swap(@stack)
+      when 'ROT'
+        StackManipulation.rot(@stack)
+      when 'ROLL'
+        StackManipulation.roll(@stack)
+      when 'ROLLD'
+        StackManipulation.rolld(@stack)
+      when 'IFELSE'
+        BooleanOperations.if_else(@stack)
+      when '==', '!=', '>', '<', '>=', '<=', '<=>'
+        BooleanOperations.handle_comparison(@stack, element)
+      else
+        puts "Unknown command: #{element}"
+      end
+    end
+    @stack
+  end
+
+
+  private
+
+  def extract_lambda_tokens
+    # Logic to extract lambda tokens from the stack
+    []
+  end
+end
+
 module Arithmetic
   def self.handle_operation(stack, operator)
     # Handle binary operations
     if %w[+ - * / ** % & | ^ << >>].include?(operator)
       b = stack.pop
       a = stack.pop
-      stack.push(a.send(operator, b))
+
+      a = a.to_i if a.is_a?(Integer)
+      b = b.to_i if b.is_a?(Integer)
+      result = a.send(operator, b)
+      stack.push(result)
     elsif operator == '!'
       stack.push(!stack.pop)
     elsif operator == '~' # One's complement
@@ -12,8 +70,6 @@ module Arithmetic
     end
   end
 end
-
-
 
 module StackManipulation
   def self.drop(stack)
@@ -30,10 +86,8 @@ module StackManipulation
   end
 
   def self.rot(stack)
-    a = stack.pop
-    b = stack.pop
-    c = stack.pop
-    stack.push(b, a, c)
+    a, b, c = stack.pop(3)
+    stack.push(b, c, a)
   end
 
   def self.roll(stack)
@@ -44,10 +98,8 @@ module StackManipulation
 
   def self.rolld(stack)
     n = stack.pop
-    if n > 0 && n <= stack.length
-      elements_to_rotate = stack.pop(n)
-      stack.push(elements_to_rotate[-1], *elements_to_rotate[0..-2])
-    end
+    elements_to_rotate = stack.pop(n)
+    stack.push(elements_to_rotate[-1], *elements_to_rotate[0..-2])
   end
 end
 
@@ -56,11 +108,7 @@ module BooleanOperations
     condition = stack.pop
     false_case = stack.pop
     true_case = stack.pop
-    if condition != 0 && condition != false
-      stack.push(true_case)
-    else
-      stack.push(false_case)
-    end
+    stack.push(condition ? true_case : false_case)
   end
 
   def self.handle_comparison(stack, operator)
@@ -79,66 +127,39 @@ module BooleanOperations
   end
 end
 
+
 if __FILE__ == $0
   input_file = ARGV[0]
   unless input_file
-    puts "Usage: ruby ws.rb input-xxx.txt"
+    puts "Usage: ruby calculator.rb input-xxx.txt"
     exit 1
   end
 
   digits = input_file.match(/input-(\d{3})\.txt/)[1]
   output_file = File.join('output', "output-#{digits}.txt")
 
-  stack = []
-
+  tokens = []
   begin
-    # Read input file
+    # Load the tokens from the input file
     File.open(input_file, 'r') do |file|
       file.each_line do |line|
-        elements = line.scan(/"[^"]*"|\S+/) # extract quoted strings and standalone tokens
-        elements.each do |element|
-          case element
-          when /\d+/ # Integer
-            stack.push(element.to_i)
-          when '+', '-', '*', '/', '**', '%', '&', '|', '^', '<<', '>>', '!', '~'
-            Arithmetic.handle_operation(stack, element)
-          when 'DROP'
-            StackManipulation.drop(stack)
-          when 'DUP'
-            StackManipulation.dup(stack)
-          when 'SWAP'
-            StackManipulation.swap(stack)
-          when 'ROT'
-            StackManipulation.rot(stack)
-          when 'ROLL'
-            StackManipulation.roll(stack)
-          when 'ROLLD'
-            StackManipulation.rolld(stack)
-          when 'IFELSE'
-            BooleanOperations.if_else(stack)
-          when '==', '!=', '>', '<', '>=', '<=', '<=>'
-            BooleanOperations.handle_comparison(stack, element)
-          when 'true'
-            stack.push(true)
-          when 'false'
-            stack.push(false)
-          when /\A".*"\z/ # String
-            stack.push(element[1..-2]) # remove quotes
-          else
-            puts "Unknown command: #{element}"
-          end
+        line.scan(/"[^"]*"|\S+/).each do |token|
+          # Check if the token is an integer using a regular expression and convert if true
+          parsed_token = token.match?(/\A-?\d+\z/) ? token.to_i : token
+          tokens << parsed_token
         end
       end
     end
 
-    # Write stack to output file
+    calculator = Calculator.new(tokens)
+    stack = calculator.evaluate
+
     File.open(output_file, 'w') do |file|
       stack.each do |element|
         formatted_element = element.is_a?(String) ? "\"#{element}\"" : element
         file.puts(formatted_element)
       end
     end
-
   rescue => e
     puts "Error: #{e.message}"
   end
